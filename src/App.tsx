993a1cf4c22bf87b2370075fc2df526ea7f69406
minjaehan <tommy3942@naver.com>
2025-07-02 01:12:24 +0900

    chore: 스타일 정상 적용 확인문구 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
-  return (  
+  return (
     <>
-   
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold mb-4">Tailwind for Snack</h1>
+        <p className="text-lg text-blue-700">
+          패키지 설치 후 정상적으로 적용되는지 확인 바랍니다.{" "}
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
