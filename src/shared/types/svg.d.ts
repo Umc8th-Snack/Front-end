0ff1c3d35c4f5a30f04de3216d8185a1f696b4db
sooloin <csb7543@naver.com>
2025-07-07 00:38:01 +0900

    chore: SVGR 설정 제거(별도 브랜치에서 재구성 예정)

--- src/shared/types/svg.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.svg' {
-    import * as React from 'react';
-
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
-} //.svg 파일을 불러올 때 ReactComponent로 사용할 수 있다고 명시
