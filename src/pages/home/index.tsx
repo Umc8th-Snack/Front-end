2e032fe114d3b7e4677f1d26177d20ad7e2eaa73
seojeongm <seojeongm@users.noreply.github.com>
2025-07-09 23:56:24 +0900

    chore: 디자인 시스템 테스트용 임시 코드 삭제

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,7 @@
-import FontTest from '../FontTest';
-
 const HomePage = () => {
     return (
         <div className="flex min-h-screen items-center justify-center">
-            {/* TODO: FontTest는 임시로 넣은 컴포넌트입니다. 추후 삭제 예정입니다. */}
-            {/* <h1 className="text-4xl font-bold">홈 페이지</h1> */}
-            <FontTest />
+            <h1 className="text-4xl font-bold">홈 페이지</h1>
         </div>
     );
 };
