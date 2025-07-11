f98ce37fe249edbf02ceadc1078c387bac1f0e26
minjaehan <tommy3942@naver.com>
2025-07-11 17:06:28 +0900

    fix:에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), svgr()],
+    plugins: [react(), svgr(), tailwindcss()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
