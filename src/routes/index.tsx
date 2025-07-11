ebdb37c632037b160e269bb83bb8b13216e39d87
Kyoungho Eom <kheom0512@gmail.com>
2025-07-11 16:23:37 +0900

    fix: 페이지 import 경로 오류 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,11 +6,11 @@ import MainLayout from '@/layout/MainLayout';
 import LoadingFallback from './LoadingFallback';
 import ProtectedRoute from './ProtectedRoute';
 
-const HomePage = lazy(() => import('@/pages/home'));
-const LoginPage = lazy(() => import('@/pages/login'));
-const SignupPage = lazy(() => import('@/pages/signup'));
-const ArticlePage = lazy(() => import('@/pages/article'));
-const MyPage = lazy(() => import('@/pages/mypage'));
+const HomePage = lazy(() => import('@/pages/home/HomePage'));
+const LoginPage = lazy(() => import('@/pages/login/LoginPage'));
+const SignupPage = lazy(() => import('@/pages/signup/SignupPage'));
+const ArticlePage = lazy(() => import('@/pages/article/ArticlePage'));
+const MyPage = lazy(() => import('@/pages/my-page/MyPage'));
 
 const routes: RouteObject[] = [
     {
