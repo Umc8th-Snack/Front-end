29c2d79f744bc187a3fe869afb7eecfb99a32cde
minjaehan <tommy3942@naver.com>
2025-07-19 22:31:59 +0900

    feat: 간추린 뉴스 박스 UI 구현

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,9 @@
-import FontTest from '../FontTest';
+import SummerizedNews from '@/shared/components/SummarizedNews/SummarizedNews';
 
 const HomePage = () => {
     return (
-        <div className="flex min-h-screen items-center justify-center">
-            {/* TODO: FontTest는 임시로 넣은 컴포넌트입니다. 추후 삭제 예정입니다. */}
-            {/* <h1 className="text-4xl font-bold">홈 페이지</h1> */}
-            <FontTest />
-            <p className="mt-4 text-lg">actions sync 확인</p>
+        <div className="home-page">
+            <SummerizedNews />
         </div>
     );
 };
