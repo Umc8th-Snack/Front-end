b1d746ab1846498bf8ab369cdc4cd772bee85255
minjaehan <tommy3942@naver.com>
2025-07-25 22:56:33 +0900

    fix: 디자인 토큰에 맞게 수정

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,11 +1,5 @@
-import MemoPad from '@/shared/components/MemoPad/MemoPad';
-
 const HomePage = () => {
-    return (
-        <div className="relative flex min-h-screen items-center justify-center">
-            <MemoPad />
-        </div>
-    );
+    return <div className="relative flex min-h-screen items-center justify-center"></div>;
 };
 
 export default HomePage;
