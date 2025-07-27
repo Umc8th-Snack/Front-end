36b944855fdeef97967b25cf9b94a47fee9f7a3f
sooloin <csb7543@naver.com>
2025-07-27 15:01:41 +0900

    design, style: 컴포넌트명 변경 및 디자인 시스템 적용

--- src/shared/components/InputBox/InputBox.tsx
+++ src/shared/components/InputBox/InputBox.tsx
@@ -4,16 +4,16 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label?: string;
 }
 
-const Input = ({ label, className, ...props }: InputProps) => {
+const InputBox = ({ label, className, ...props }: InputProps) => {
     return (
         <div className="my-8 flex w-full flex-col gap-2">
-            {label && <label className="text-[20px] font-medium text-gray-700">{label}</label>}
+            {label && <label className="text-24px-medium text-gray-700">{label}</label>}
             <input
                 {...props}
-                className={`rounded-md border border-[#B2B2B2] px-4 py-3 text-[20px] transition placeholder:text-[#B2B2B2] hover:border-blue-400 focus:ring-2 focus:ring-blue-400 focus:outline-none ${className ?? ''}`}
+                className={`text-24px-medium hover:border-main focus:ring-main rounded-md border border-[#B2B2B2] px-4 py-3 transition placeholder:text-[#B2B2B2] focus:ring-1 focus:outline-none ${className ?? ''}`}
             />
         </div>
     );
 };
 
-export default Input;
+export default InputBox;
