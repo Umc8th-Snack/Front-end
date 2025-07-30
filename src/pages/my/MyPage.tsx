58984fcc6c273beb8a6bcbb44d798b2caa90c949
minjaehan <tommy3942@naver.com>
2025-07-30 17:52:11 +0900

    feat: 텝에 cursor-pointer 추가

--- src/pages/my/MyPage.tsx
+++ src/pages/my/MyPage.tsx
@@ -34,7 +34,7 @@ const MyPage = () => {
             {/* 탭 메뉴 */}
             <div className="mb-4 flex">
                 <button
-                    className={`text-24px-semibold flex-1 border-b-2 py-1 text-center transition-colors duration-200 ${
+                    className={`text-24px-semibold flex-1 cursor-pointer border-b-2 py-1 text-center transition-colors duration-200 ${
                         tab === 'memo' ? 'border-main text-main' : 'border-black-30 text-black-30'
                     }`}
                     onClick={() => setTab('memo')}
@@ -42,7 +42,7 @@ const MyPage = () => {
                     메모장
                 </button>
                 <button
-                    className={`text-24px-semibold flex-1 border-b-2 py-1 text-center transition-colors duration-200 ${
+                    className={`text-24px-semibold flex-1 cursor-pointer border-b-2 py-1 text-center transition-colors duration-200 ${
                         tab === 'scrap' ? 'border-main text-main' : 'border-black-30 text-black-30'
                     }`}
                     onClick={() => setTab('scrap')}
