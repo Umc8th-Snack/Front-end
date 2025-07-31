4a34497de4c2f6c4c8f25d0324c2e065ed449128
minjaehan <tommy3942@naver.com>
2025-08-01 04:44:06 +0900

    fix: 파일구조 변경

---
+++ src/pages/settings/EmailChangePage.tsx
@@ -0,0 +1,9 @@
+const EmailChangePage = () => {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">이메일 변경 페이지</h1>
+        </div>
+    );
+};
+
+export default EmailChangePage;
