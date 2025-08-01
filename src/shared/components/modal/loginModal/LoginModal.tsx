85168a83c786e7c365d6cc3ad61deb5c0e465dfb
Kyoungho Eom <kheom0512@gmail.com>
2025-08-01 19:38:18 +0900

    style: 로그인 모달 버튼 간격 및 입력 폼 스타일 수정

--- src/shared/components/modal/loginModal/LoginModal.tsx
+++ src/shared/components/modal/loginModal/LoginModal.tsx
@@ -125,7 +125,7 @@ const LoginModal = ({ isOpen, onClose }: ModalProps) => {
                         {/* 이메일 로그인 폼 */}
                         <div className="mt-18 px-12">
                             {/* 입력 필드 */}
-                            <div className="flex flex-col gap-4 pb-2">
+                            <div className="flex flex-col gap-2">
                                 <InputBox
                                     label="이메일"
                                     name="email"
@@ -148,7 +148,7 @@ const LoginModal = ({ isOpen, onClose }: ModalProps) => {
                                 <button
                                     onClick={handleLoginSubmit}
                                     disabled={!isFormValid}
-                                    className={`text-20px-medium mt-5 h-[56px] w-full rounded-md py-3 text-white ${
+                                    className={`text-20px-medium mt-8 h-[56px] w-full rounded-md py-3 text-white ${
                                         isFormValid ? 'bg-main cursor-pointer' : 'bg-black-30 cursor-not-allowed'
                                     }`}
                                 >
