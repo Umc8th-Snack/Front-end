2aa20b68e5b849f17f74faf0ae8baf2c144acb99
Kyoungho Eom <kheom0512@gmail.com>
2025-08-01 20:06:33 +0900

    style: 입력 라벨 색상을 검정으로 변경

--- src/shared/components/box/InputBox/InputBox.tsx
+++ src/shared/components/box/InputBox/InputBox.tsx
@@ -7,7 +7,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const InputBox = ({ label, className, ...props }: InputProps) => {
     return (
         <div className="my-1 flex w-full flex-col gap-2">
-            {label && <label className="text-18px-medium text-black-70">{label}</label>}
+            {label && <label className="text-18px-medium text-black">{label}</label>}
             <input
                 {...props}
                 className={`text-18px-medium hover:border-main focus:ring-main rounded-md border border-[#B2B2B2] px-4 py-3 transition placeholder:text-[#B2B2B2] focus:ring-1 focus:outline-none ${className ?? ''}`}
