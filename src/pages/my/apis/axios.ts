6174f8465ffcaf767d50079d285a02c46d59285b
minjaehan <tommy3942@naver.com>
2025-08-08 04:41:24 +0900

    fix: axios 임시 토큰  Authorization 추가

--- src/pages/my/apis/axios.ts
+++ src/pages/my/apis/axios.ts
@@ -5,10 +5,12 @@ const axiosInstance = axios.create({
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
+        //임시로 직접 넣음
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsInVzZXJJZCI6MTIsImVtYWlsIjoibXlwYWdldGVzdEBuYXZlci5jb20iLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNzU0NTk0OTgyLCJleHAiOjE3NTQ1OTY3ODJ9.GAP6b3e1A1r_xMTjGPikwN1jJuLy5olyiSLFlvXIaEA`,
     },
 });
 
-// 필요시 토큰 추가
+//로컬 스토리지 사용 (로그인 기능 추가 후 변경 필요?)
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('accessToken');
     if (token) {
