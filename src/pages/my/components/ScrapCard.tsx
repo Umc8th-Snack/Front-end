83d2b70a66c5fd919833fdefe332da699ec4a273
minjaehan <tommy3942@naver.com>
2025-08-08 04:44:09 +0900

    feat: scrapCard 변수 명 변경 및 글자 사이즈 미세 변경

--- src/pages/my/components/ScrapCard.tsx
+++ src/pages/my/components/ScrapCard.tsx
@@ -1,7 +1,7 @@
-const ScrapCard = ({ title, summary: excerpt }: { title: string; summary: string }) => (
+const ScrapCard = ({ title, summary }: { title: string; summary: string }) => (
     <div className="border-main-70 h-[172px] w-[672px] rounded-[8px] border p-4">
-        <div className="text-24px-medium text-black">{title}</div>
-        <p className="text-20px-medium text-black-70 mt-2">{excerpt}</p>
+        <div className="text-20px-bold text-black">{title}</div>
+        <p className="text-20px-medium text-black-70 mt-2">{summary}</p>
     </div>
 );
 
