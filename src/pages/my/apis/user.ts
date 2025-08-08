45155a33b227bfb9a999b568d91a11d40f82c7ec
minjaehan <tommy3942@naver.com>
2025-08-08 18:35:31 +0900

    feat: 프로필 수정 페이로드 및 api 요청 함수

--- src/pages/my/apis/user.ts
+++ src/pages/my/apis/user.ts
@@ -1,8 +1,18 @@
+// src/pages/my/apis/user.ts
 import axiosInstance from '@/pages/my/apis/axios';
 import type { UserProfile } from '@/pages/my/types/types';
 
 export const fetchUserProfile = async (): Promise<UserProfile> => {
     const res = await axiosInstance.get('/api/users/me');
+    return res.data.result;
+};
 
+export type UpdateUserProfilePayload = {
+    nickname?: string;
+    introduction?: string;
+};
+
+export const updateUserProfile = async (payload: UpdateUserProfilePayload): Promise<UserProfile> => {
+    const res = await axiosInstance.patch('/api/users/me', payload);
     return res.data.result;
 };
