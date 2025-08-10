a3b699a2143e7c70bf07f8032cb942e12fcc2af5
minjaehan <tommy3942@naver.com>
2025-08-11 00:45:30 +0900

    fix: 파일구조 수정

--- src/pages/settings/SettingsDropdown/SettingsDropdown.tsx
+++ src/pages/settings/SettingsDropdown/SettingsDropdown.tsx
@@ -1,8 +1,9 @@
-// SettingsDropdown.tsx
+// src/pages/my/components/SettingsDropdown.tsx
 import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { settingsData } from './settingsData';
+import { getSettingsData } from '@/pages/settings/SettingsDropdown/settingsData';
+
 import { useOutsideClick } from './useOutsideClick';
 
 interface SettingsDropdownProps {
@@ -14,6 +15,7 @@ interface SettingsDropdownProps {
 const SettingsDropdown = ({ open, setOpen, onShowConsentModal }: SettingsDropdownProps) => {
     const dropdownRef = useRef<HTMLDivElement>(null!);
     const navigate = useNavigate();
+    const settingsData = getSettingsData(navigate);
 
     useOutsideClick(dropdownRef, () => setOpen(false));
 
@@ -21,7 +23,7 @@ const SettingsDropdown = ({ open, setOpen, onShowConsentModal }: SettingsDropdow
 
     return (
         <div className="relative" ref={dropdownRef}>
-            <div className="border-black-30 absolute top-full right-0 z-50 mt-2 flex h-[431px] w-[320px] flex-col justify-center rounded-[10px] border border-[0.5px] bg-white p-6 shadow-[-4px_4px_8px_0px_rgba(0,0,0,0.15)]">
+            <div className="border-black-30 absolute top-full right-0 z-50 mt-2 flex h-[431px] w-[320px] flex-col justify-center rounded-[10px] border-[0.5px] bg-white p-6 shadow-[-4px_4px_8px_0px_rgba(0,0,0,0.15)]">
                 <h2 className="text-24px-semibold mb-10 text-center">설정</h2>
                 {settingsData.map((section, i) => (
                     <div key={section.category} className={i === 0 ? '' : 'mt-6'}>
@@ -35,7 +37,7 @@ const SettingsDropdown = ({ open, setOpen, onShowConsentModal }: SettingsDropdow
                                             if (item.label === '정보 동의 설정') {
                                                 onShowConsentModal?.();
                                             } else if (item.path) void navigate(item.path);
-                                            else if (item.onClick) item.onClick();
+                                            else if (item.onClick) void item.onClick();
                                         }}
                                         className="text-18px-medium text-black-50 w-full cursor-pointer text-left transition-colors hover:text-black"
                                     >
