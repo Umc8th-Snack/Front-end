17c7b92c1a744ad76cea7773d8a088e9afa42e08
minjaehan <tommy3942@naver.com>
2025-08-11 19:10:19 +0900

    feat: 회원 탈퇴 api 호출 함수

--- src/pages/settings/apis/auth.ts
+++ src/pages/settings/apis/auth.ts
@@ -3,3 +3,10 @@ import axiosInstance from '@/pages/my/apis/axios';
 export const logout = async (): Promise<void> => {
     await axiosInstance.post('/api/auth/logout');
 };
+
+export const DeleteAccount = async (password: string): Promise<void> => {
+    const res = await axiosInstance.post('/api/users/me/withdraw', { password });
+    if (res.data && res.data.isSuccess === false) {
+        throw new Error(res.data.message);
+    }
+};
