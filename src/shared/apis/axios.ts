d9250584831089092c126835bdc44a552361a675
minjaehan <tommy3942@naver.com>
2025-08-15 02:54:08 +0900

    fix:프로필 이미지 버그 수정

--- src/shared/apis/axios.ts
+++ src/shared/apis/axios.ts
@@ -1,25 +1,20 @@
+// src/shared/apis/axios.ts
 import type { AxiosInstance } from 'axios';
 import axios from 'axios';
 
-import { API_CONFIG, API_TIMEOUT, CONTENT_TYPE } from '../constants/apiConstants';
+import { API_CONFIG, API_TIMEOUT } from '../constants/apiConstants';
 import { handleRequestError, handleRequestSuccess } from '../utils/interceptors/requestInterceptor';
 import { handleResponseError, handleResponseSuccess } from '../utils/interceptors/responseInterceptor';
 
-// Axios 인스턴스 생성
+// ⚠️ 전역 Content-Type 제거 (중요)
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: API_CONFIG.BASE_URL,
     timeout: API_TIMEOUT,
-    headers: {
-        'Content-Type': CONTENT_TYPE,
-    },
     withCredentials: API_CONFIG.WITH_CREDENTIALS,
     maxRedirects: API_CONFIG.MAX_REDIRECTS,
 });
 
-// Request 인터셉터 등록
 axiosInstance.interceptors.request.use(handleRequestSuccess, handleRequestError);
-
-// Response 인터셉터 등록
 axiosInstance.interceptors.response.use(handleResponseSuccess, handleResponseError);
 
 export default axiosInstance;
