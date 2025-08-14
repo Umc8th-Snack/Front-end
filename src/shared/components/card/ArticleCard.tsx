59c815ec6f1eb7beee617a2349f97db319876386
sooloin <csb7543@naver.com>
2025-08-14 12:22:32 +0900

    design: article card 디자인 디테일 변경

--- src/shared/components/card/ArticleCard.tsx
+++ src/shared/components/card/ArticleCard.tsx
@@ -8,8 +8,8 @@ interface ArticleCardProps {
 function ArticleCard({ title = 'article title', imageUrl, size = 'sidebar' }: ArticleCardProps) {
     // 크기에 따른 스타일 클래스
     const sizeClasses = {
-        main: 'h-[210px] w-[255px] p-6 m-2', // 메인 페이지용 - 더 큰 크기, 작은 margin
-        sidebar: 'h-[168px] w-[204px] p-6 m-5', // sidebar용 - 기존 크기, 기존 margin
+        main: 'h-[210px] w-[255px] p-6', // 메인 페이지용 - 더 큰 크기, 작은 margin
+        sidebar: 'h-[168px] w-[204px] p-6', // sidebar용 - 기존 크기, 기존 margin
     };
 
     const imageSizeClasses = {
@@ -19,7 +19,7 @@ function ArticleCard({ title = 'article title', imageUrl, size = 'sidebar' }: Ar
 
     return (
         <div
-            className={`bg-main-10 m-5 flex flex-col rounded-tl-[22px] rounded-tr-[8px] rounded-br-[22px] rounded-bl-[8px] ${sizeClasses[size]}`}
+            className={`bg-main-10 my-2 flex flex-col rounded-tl-[22px] rounded-tr-[8px] rounded-br-[22px] rounded-bl-[8px] ${sizeClasses[size]}`}
         >
             {/* 카테고리 아이콘 또는 이미지 */}
             <div className="mb-2 flex w-full justify-start">
