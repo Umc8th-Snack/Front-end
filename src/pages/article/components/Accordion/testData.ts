70251da7d3042c070beb6cc9cab2a83b0cbee07b
minjaehan <tommy3942@naver.com>
2025-08-15 00:30:33 +0900

    fix: 배포 오류 수정(아코디언 testData id 입력)

--- src/pages/article/components/Accordion/testData.ts
+++ src/pages/article/components/Accordion/testData.ts
@@ -3,11 +3,13 @@ import type { QuizItem } from '@/pages/article/types/accordionTypes';
 
 export const quizData: QuizItem[] = [
     {
+        id: 1,
         question: '이번 프로젝트의 서비스 명은?',
         options: ['스넉', '스낵', '스낵면', '스웩'],
         answer: 0,
     },
     {
+        id: 2,
         question: '지금은 무슨 계절인가요?',
         options: ['봄', '여름', '가을', '겨울'],
         answer: 0,
