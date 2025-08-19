e1d71dd8178044b72e26291e4a6bb2670ed6cc35
sooloin <csb7543@naver.com>
2025-08-19 14:04:18 +0900

    design: ArticleLayout 하드코딩 수정

--- src/layout/ArticleLayout.tsx
+++ src/layout/ArticleLayout.tsx
@@ -10,14 +10,14 @@ interface ArticleLayoutProps {
 const ArticleLayout = ({ children, sidebarContent }: ArticleLayoutProps) => {
     return (
         <div className="px-6 py-6">
-            <div className="mx-auto grid max-w-[1100px] grid-cols-[720px_264px] gap-30">
+            <div className="mx-auto grid max-w-[1100px] grid-cols-[70%_30%]">
                 <div>
                     <div className="flex flex-col gap-4 px-6 py-6">{children}</div>
                 </div>
 
                 {/* Sidebar */}
                 <aside className="mt-8">
-                    <div className="sticky top-15">
+                    <div className="sticky top-15 flex justify-end">
                         {sidebarContent || <RelatedArticleList onClose={() => {}} articleId={0} />}
                     </div>
                 </aside>
