ba895b092b1262477bfa45d2473a432680256d21
minjaehan <tommy3942@naver.com>
2025-08-19 17:18:50 +0900

    fix: 배포 오류 수정(navbar 임포트 대소문자)

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import Navbar from '@/shared/components/navbar/Navbar';
+import Navbar from '@/shared/components/Navbar/Navbar';
 
 const MainLayout = () => {
     return (
