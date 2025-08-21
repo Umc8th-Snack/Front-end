41084919f4b4331ef5009b3785f2a7bd5868ccd0
Kyoungho Eom <kheom0512@gmail.com>
2025-08-21 23:57:55 +0900

    fix: delete 메서드에 params 옵션 추가

--- src/shared/apis/api.ts
+++ src/shared/apis/api.ts
@@ -90,6 +90,7 @@ const api = {
      */
     delete: async <T = unknown>(url: string, options?: ApiRequestOptionsTypes): Promise<T> => {
         const response: AxiosResponse<ApiResponseTypes<T>> = await axiosInstance.delete(url, {
+            params: options?.params,
             headers: options?.headers,
             timeout: options?.timeout,
         });
