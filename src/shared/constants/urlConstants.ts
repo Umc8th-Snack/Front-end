00218fc84e1f3cbd23f6d53009ac7bbdc4769b3f
minjaehan <tommy3942@naver.com>
2025-11-05 23:41:43 +0900

    최종수정

--- src/shared/constants/urlConstants.ts
+++ src/shared/constants/urlConstants.ts
@@ -1,1 +1,1 @@
-export const SNACKER_NOTION_URL = 'https://cheddar-gooseberry-e64.notion.site/snack-umc8th-2025';
+export const SNACKER_NOTION_URL = 'https://cheddar-gooseberry-e64.notion.site/snack';
